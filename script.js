f6aba0f3ebf7b8d989780e28b98304e581051187
Wehlia <wehliakahin@gmail.com>
2025-08-13 23:16:54 +0100

    Added profile image

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+/* Vertically scrolling image gallery code:
+This code will allow scrolling between images using mouse wheel or a mousepad
+*/
 let scrollContainer = document.querySelector(".gallery");
     let leftBtn = document.getElementById("leftBtn");
     let rightBtn = document.getElementById("rightBtn");
@@ -6,6 +8,18 @@ let scrollContainer = document.querySelector(".gallery");
     scrollContainer.addEventListener("wheel", (evt) => {
         evt.preventDefault();
         scrollContainer.scrollLeft += evt.deltaY;
+        scrollContainer.style.scrollBehavior = "auto";
 
-    })
+    });
+
+    rightBtn.addEventListener("click", () =>{
+        scrollContainer.style.scrollBehavior = "smooth";
+        scrollContainer.scrollLeft += +900;
+
+    });
+
+    leftBtn.addEventListener("click", () =>{
+        scrollContainer.style.scrollBehavior = "smooth";
+        scrollContainer.scrollLeft -= +900;
+    });
 
